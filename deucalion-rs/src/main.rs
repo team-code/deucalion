4a272c2f72f41cfd71e861ca9924f23b621a603b
SilverWingedSeraph <lfstindall@gmail.com>
2016-07-07 08:14:53 -0700

    Set up fakemain.

--- deucalion-rs/src/main.rs
+++ deucalion-rs/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // Fake-main technique. This allows the main() function to return an int (i32) status code.
+    std::process::exit(fake_main());
+}
+
+fn fake_main() -> i32 {
+    println!("Hello from fakemain.");
+    return 0;
 }
