12f50c0e44117052e8354f7d12c2ec54cb5869c6
SilverWingedSeraph <lfstindall@gmail.com>
2016-07-07 15:50:08 -0700

    Reorganize main.rs

--- deucalion-rs/src/main.rs
+++ deucalion-rs/src/main.rs
@@ -1,9 +1,9 @@
-fn main() {
-    // Fake-main technique. This allows the main() function to return an int (i32) status code.
-    std::process::exit(fake_main());
-}
-
 fn fake_main() -> i32 {
     println!("Hello from fakemain.");
     return 0;
+}
+
+fn main() {
+    // Fake-main technique. This allows the main() function to return an int (i32) status code.
+    std::process::exit(fake_main());
 }
