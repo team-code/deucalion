6a750ab6c6d4c16a3b4ab82f48d094f7dbe4f510
SilverWingedSeraph <lfstindall@gmail.com>
2016-07-09 09:28:42 -0700

    Remove all the unused files from the old version.

--- data/engine_config.lua
+++
@@ -1,9 +0,0 @@
--- Engine Configuration File engine_config.lua
-
--- This file holds all configuration that is not set by the developer, such as 
---	resolution, framerate, savegame directory
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
-
-MAXIMUM_FRAMERATE = 60
-
