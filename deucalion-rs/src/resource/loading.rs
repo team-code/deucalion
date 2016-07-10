c64a2d75a53b0fb66742454a0cb75cd76dd95b76
SilverWingedSeraph <lfstindall@gmail.com>
2016-07-10 15:13:08 -0700

    Add get_resource_reader_by_name()

--- deucalion-rs/src/resource/loading.rs
+++ deucalion-rs/src/resource/loading.rs
@@ -4,6 +4,17 @@ use std::path::{Path, PathBuf};
 use resource::ResourceKind;
 use error::DeucalionError;
 
+use std::fs::File;
+
+/// Get a reader that can read in the requested resource from disk.
+/// If requesting a resource whose name doesn't change, the `name` argument is ignored.
+pub fn get_resource_reader_by_name(kind: ResourceKind, name: &str) -> Result<File, DeucalionError> {
+    // Acquire the path to the resource
+    let resource_path = try!(get_resource_path_by_name(kind, name));
+    let reader = try!(File::open(&resource_path));
+    return Ok(reader);
+}
+
 /// Get the path to a resource in the data directory scheme. See BLUEPRINT.md for more info.
 /// If requesting a resource whose name doesn't change, the `name` argument is ignored.
 pub fn get_resource_path_by_name(kind: ResourceKind,
--- deucalion-rs/src/resource/test_loading.rs
+++ deucalion-rs/src/resource/test_loading.rs
@@ -1,6 +1,9 @@
-use resource::loading::get_resource_path_by_name;
+use resource::loading::*;
 use resource::ResourceKind;
 use std::path::PathBuf;
+
+use std::io::prelude::*;
+use std::fs::File;
 
 #[test]
 // Make sure that get_resource_path_by_name can properly generate the names for maps
@@ -15,11 +18,32 @@ fn test_map_path() {
 
 #[test]
 // Make sure that get_resource_path_by_name can properly generate the name of the engine config
-fn test_loading() {
+fn test_engine_config_path() {
     let correct_path = PathBuf::from("./data/engine_config.lua");
     let result = match get_resource_path_by_name(ResourceKind::EngineConfig, "Doesn't matter~~") {
         Ok(v) => v,
         Err(e) => panic!("Error: {}", e),
     };
     assert_eq!(correct_path, result);
+}
+
+#[test]
+// Make sure that get_resource_reader_by_name is able to acquire readers for resources
+fn test_engine_config_reader() {
+    let mut correct_content = String::new();
+    let mut test_content = String::new();
+
+    // Read it in "manually"
+    File::open(get_resource_path_by_name(ResourceKind::EngineConfig, "").unwrap())
+        .unwrap()
+        .read_to_string(&mut correct_content)
+        .unwrap();
+
+    // Read it in with get_resource_reader_by_name
+    get_resource_reader_by_name(ResourceKind::EngineConfig, "")
+        .unwrap()
+        .read_to_string(&mut test_content)
+        .unwrap();
+
+    assert_eq!(correct_content, test_content);
 }
