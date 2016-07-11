00fdba425b285abc5326720ed1edd59c0d4d6a95
SilverWingedSeraph <lfstindall@gmail.com>
2016-07-11 08:47:23 -0700

    Add sample game config file

---
+++ deucalion-rs/data/game_config.lua
@@ -0,0 +1,7 @@
+-- Game Configuration File game_config.lua
+
+-- Information about the game, set by the developer, such as title,
+-- starting map, et cetera.
+
+TITLE = "Deucalion Engine"
+STARTING_MAP = "map001"
